1ab94601daaa4311b7b3c956540971c60f1e290a
dbqudgns <dbqudgns4632@gmai.com>
2025-01-27 18:06:46 +0900

    2025/01/27 Spring_JPA-1

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items") //하나의 상품은 여러 카테고리에 속할 수 있다.
+    private List<Category> categories = new ArrayList<>();
+
+}
