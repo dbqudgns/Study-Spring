662f96e2a2e53e21f2645eb43204b2eb5cae9dd0
dbqudgns <dbqudgns4632@gmail.com>
2025-06-20 16:53:14 +0900

    dev : 주문 도메인 개발

---
+++ Spring_JPA-Dev-1/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,68 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+/**
+ * @Transactional(readOnly = true)을 안 써도 Lazy 로딩은 유지된다.
+ * 하지만 트랜잭션이 없으면 지연 로딩 중에 예외(트랜잭션 설정을 안해서 프록시 객체가 없어 DB와 연결이 끊김)가 나기 때문에,
+ * 조회 메서드에도 @Transactional(readOnly = true)를 쓰는 게 안전하다. */
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /** 주문 */
+    @Transactional
+    public Long order (Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        /** Order 엔티티에서 Delivery와 OrderItem에 Cascade.ALL로 설정을 하였기에
+         /* Order 저장 시 Delivery와 OrderItem도 persist 된다 ! 즉, Repository가 필요없다.
+         */
+
+        return order.getId();
+    }
+
+    /** 주문 취소 */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    /** 주문 검색 */
+
+
+
+}
