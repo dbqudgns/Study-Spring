268fbd403fcea01bd91bbe8c7d72ab0b0e83df40
dbqudgns <dbqudgns4632@gmail.com>
2025-06-30 17:55:40 +0900

    dev : 공통 인터페이스(JpaRepository) 적용 및 분석

--- Spring_Data_JPA/src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ Spring_Data_JPA/src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -5,15 +5,42 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
     @PersistenceContext // Spring이 영속성 컨텍스트가 연결된 EntityManager를 자동 주입해주는 어노테이션
     private EntityManager em; // EntityManager : 영속성 컨텍스트를 직접 제어할 수 있는 객체
 
+    // 저장
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    // 삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // 단건 조회 : Optional 반환
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    // 카운트
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 
     public Member find(Long id) {
