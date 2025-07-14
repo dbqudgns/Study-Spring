58669e0a917199667c631b2c40258852fe2259e9
dbqudgns <dbqudgns4632@gmail.com>
2025-07-14 16:49:10 +0900

    dev : 쿼리 메서드 기능 - 순수 JPA 페이징과 정렬

--- Spring_Data_JPA/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ Spring_Data_JPA/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -79,5 +79,33 @@ class MemberJpaRepositoryTest {
         assertThat(result.size()).isEqualTo(1);
     }
 
+    // JPA 페이징 처리 및 개수 반환 테스트
+    @Test
+    void paging() throws Exception {
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        // 페이지 계산 공식 적용
+        // totalPage(마지막 페이지) = totalCount / limit
+        // firstPage(최초 페이지) = offset + 1  => offset이 0이라는 가정 하에
+
+        //then
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+
+    }
+
 
 }
