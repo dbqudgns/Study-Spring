b1fcab0db394d1ef326a3ac407c8bef016c2b0d3
dbqudgns <dbqudgns4632@gmail.com>
2025-07-22 14:53:32 +0900

    dev : 기본 문법 - 조인 : ON

--- Spring_QueryDSL/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ Spring_QueryDSL/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -279,8 +279,51 @@ public class QuerydslBasicTest {
         assertThat((result))
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
+    }
 
+    /** 조인 - on : 조인 대상 필터링
+     * ex. 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조회, 회원은 모두 조회
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
 
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
 
+        List<Tuple> result2 = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result2) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /** 조인 - on : 연관관계 없는 엔티티 외부 조인
+     * ex. 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name)) // id 매핑식이 들어가지 않는다.
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("t=" + tuple);
+        }
     }
 }
