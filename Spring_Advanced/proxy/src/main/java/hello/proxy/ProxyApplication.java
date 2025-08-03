f76b25e4dc4395d1c440d3ef2cc5c3ecd8519336
dbqudgns <dbqudgns4632@gmail.com>
2025-08-03 11:11:57 +0900

    dev: 프록시 패턴과 데코레이터 패턴 - 예제 프로젝트 만들기 V1

--- Spring_Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ Spring_Advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class) //
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // v3 내 클래스만 자동 스프링 빈 등록
 public class ProxyApplication {
 
 	public static void main(String[] args) {
