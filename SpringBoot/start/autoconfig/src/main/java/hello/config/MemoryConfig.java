1ae215c8502c5c05508bcc4022fc0a61ad82e226
dbqudgns <dbqudgns4632@gmail.com>
2025-09-11 15:03:53 +0900

    dev : 자동 구성 - @Conditional

--- SpringBoot/start/autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ SpringBoot/start/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) // MemoryConfig의 실행 여부는 MemoryCondition의 matches에 따라 좌우된다.
 public class MemoryConfig {
 
     @Bean
