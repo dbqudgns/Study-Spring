7c57fc873489c3cf178e4769aac016183f8a5291
dbqudgns <dbqudgns4632@gmail.com>
2025-09-16 20:11:17 +0900

    dev : 자동 구성 라이브러리 만들기

---
+++ SpringBoot/start/memory/memory-v2/src/test/java/MemoryFinderTest.java
@@ -0,0 +1,16 @@
+import memory.Memory;
+import memory.MemoryFinder;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+
+}
