da6b668663292e9e137a6ed6f747f8ef12415875
dbqudgns <dbqudgns4632@gmail.com>
2025-09-19 16:39:22 +0900

    dev : 외부 설정 사용 - Environment

---
+++ SpringBoot/start/external-read/src/main/java/hello/config/MyDataSourceEnvConfig.java
@@ -0,0 +1,39 @@
+package hello.config;
+
+import hello.datasource.MyDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.time.Duration;
+import java.util.List;
+
+/**
+ * 단점 : Environment를 직접 주입받고, env.getProperty(key)를 통해서 값을 꺼내는 과정을 반복해야 한다.
+ */
+@Slf4j
+@Configuration
+public class MyDataSourceEnvConfig {
+
+    private final Environment env;
+
+    public MyDataSourceEnvConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public MyDataSource myDataSource() {
+        String url = env.getProperty("my.datasource.url");
+        String username = env.getProperty("my.datasource.username");
+        String password = env.getProperty("my.datasource.password");
+
+        // Environment.getProperty(key, Type)를 호출할 때 타입 정보를 주면 해당 타입으로 변환해준다.
+        int maxConnection = env.getProperty("my.datasource.etc.max-connection", Integer.class);
+        Duration timeout = env.getProperty("my.datasource.etc.timeout", Duration.class);
+        List<String> options = env.getProperty("my.datasource.etc.options", List.class);
+
+        return new MyDataSource(url, username, password, maxConnection, timeout, options);
+    }
+
+}
