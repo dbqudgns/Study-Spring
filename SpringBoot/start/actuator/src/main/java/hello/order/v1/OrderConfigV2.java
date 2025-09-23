b9d768c320ad7d881a1e072b120c771b586e16a2
dbqudgns <dbqudgns4632@gmail.com>
2025-09-23 19:02:44 +0900

    dev : 모니터링 메트릭 활용 - 카운터 AOP @Counted

---
+++ SpringBoot/start/actuator/src/main/java/hello/order/v1/OrderConfigV2.java
@@ -0,0 +1,22 @@
+package hello.order.v1;
+
+import hello.order.OrderService;
+import io.micrometer.core.aop.CountedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV2 {
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceV2();
+    }
+
+    // CountedAspect를 등록하면 @Counted를 인지해서 Counter를 사용하는 AOP를 적용한다.
+    @Bean
+    public CountedAspect countedAspect(MeterRegistry registry) {
+        return new CountedAspect(registry);
+    }
+}
